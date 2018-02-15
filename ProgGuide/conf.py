126e85fdfe66fd6c298b721da3d236fe5110d9f4
Ranjani Hathaway <ranjani@yottadb.com>
2018-02-15 14:10:23 -0500

    Switched theme to sphinx_rtd_theme

--- ProgGuide/conf.py
+++ ProgGuide/conf.py
@@ -16,6 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import sphinx_rtd_theme
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -26,6 +27,9 @@
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
+
+def setup(app):
+                app.add_stylesheet('css/custom.css')
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -86,20 +90,25 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+html_context = {
+                            'css_files': ['_static/custom.css'],
+                                                            }
+html_show_sourcelink = False
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 
-html_theme_options = {'stickysidebar':'true','sidebarwidth':'330','sidebarbgcolor':'#f3f3f3','relbarbgcolor':'#3b1a68','footerbgcolor':'#3b1a68', 'sidebarlinkcolor':'#3b1a68','sidebartextcolor':'#3b1a68','bodyfont':'Raleway','headfont':'Lora'}
-
+#html_theme_options = {'stickysidebar':'true','sidebarwidth':'330','sidebarbgcolor':'#f3f3f3','relbarbgcolor':'#3b1a68','footerbgcolor':'#3b1a68', 'sidebarlinkcolor':'#3b1a68','sidebartextcolor':'#3b1a68','bodyfont':'Raleway','headfont':'Lora'}
+html_theme_options = {'sticky_navigation' : 'true', 'prev_next_buttons_location':'both'}
+html_theme_path=[sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static/css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
