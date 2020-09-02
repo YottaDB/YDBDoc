3641bd6cec481567fee4d093c17fa42cff61d3b1
Joshua Nelson <joshua@yottadb.com>
2020-09-02 14:03:01 -0400

    Fix `buildall.sh` and use it to verify links in the pipeline
    Previously, it would give the following error:
    
    ```
    $ ./buildall.sh
    ~/src/yottadb/YDBDoc/AcculturationGuide ~/src/yottadb/YDBDoc
    make: *** No rule to make target 'html'.  Stop.
    cp: cannot create directory '/AcculturationGuide': Permission denied
    ~/src/yottadb/YDBDoc
    ~/src/yottadb/YDBDoc/AdminOpsGuide ~/src/yottadb/YDBDoc
    make: *** No rule to make target 'html'.  Stop.
    cp: cannot create directory '/AdminOpsGuide': Permission denied
    ~/src/yottadb/YDBDoc
    ...
    ```
    
    Now it will use the top-level `make html`, and copy each individual
    directory into a larger directory that mirrors the structure of the
    website.
    
    The `.gitlab-ci.yml` now uses `buildall.sh` to verify that the links all
    work correctly.
    
    - Use buildall.sh for CI
    - Fail the build if a command fails
    - Install rsync in the pipeline

--- buildall.sh
+++ buildall.sh
@@ -3,9 +3,25 @@
 # Automatically build each documentation guide as html and copy to common location for upload.
 # Expects one argument to specify the (existing) destination path for the copy command, e.g. `./buildall.sh build_dir`
 
-for directory in $(ls -d */); do
+set -euo pipefail
+set -v
+
+target="$(realpath "${1:-target}")"
+mkdir -p "$target"
+
+DIRECTORIES=(
+	AcculturationGuide/
+	AdminOpsGuide/
+	MessageRecovery/
+	MultiLangProgGuide/
+	ProgGuide/
+	ReleaseNotes/
+)
+
+# Copy the HTML files
+for directory in "${DIRECTORIES[@]}"; do
     pushd $directory
-    make html
-    cp -r _build/html $1/${directory%?}
+    output="$target/${directory%?}"
+    rsync --delete -lrtuv --exclude=\*.zip --exclude=.buildinfo --exclude=.nojekyll --exclude=_* . "$output"
     popd
 done
