0d3f3680b9193c0f279103fc5f3bb4fff0e2d184
Jaahanavee Sikri <jaahanavee@yottadb.com>
2023-01-02 15:47:53 -0500

    Revert "Fixed custom_script issue with redirection"
    This reverts commit 41ae382e737143b01a7a64615239733044d08aae.

--- Plugins/_static/js/custom_script.js
+++ Plugins/_static/js/custom_script.js
@@ -37,7 +37,7 @@ function setupReleaseSelector () {
 	    var currentURL = window.location.href;
 	    var currentURLSplit = currentURL.split('/');
 	    var path = currentURLSplit[currentURLSplit.length - 1];
-	    var remaining = currentURL.includes(".net") ? "//docs.yottadb.net/" : "//docs.yottadb.com/";
+	    var remaining = "//" + currentURLSplit.slice(2,currentURLSplit.length - 3).join('/') + "/";
             if (this.value) {
 		var currentSel = this.value.replace(".","");
 	        var newURL = remaining + currentSel + path;
