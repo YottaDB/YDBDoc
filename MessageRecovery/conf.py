672f46d7c961b1b016b4b7f53581ac8c04034eca
Berwyn Hoyt <berwyn@yottadb.com>
2024-12-13 16:21:24 +1100

    Allow docs to build with new versions of Sphinx
    - New Sphinx version (e.g. 8.1.3) produce a deprecation warning when `get_html_theme_path()`
    is called and the rror message states that it is unnecessary and not recommended to set
    html_theme_path at all.
    - It still builds with earlier versions of Sphinx (e.g. 7.2.6)

--- MessageRecovery/conf.py
+++ MessageRecovery/conf.py
@@ -26,8 +26,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import sphinx_rtd_theme
-
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -104,7 +102,6 @@ html_show_sourcelink = False
 html_theme_options = {"sticky_navigation": "true", "prev_next_buttons_location": "both", "logo_only": True}
 html_logo = "logo.png"
 html_favicon = "favicon.png"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
