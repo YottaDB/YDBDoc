8084468199aa4c2979f0bf8d9c2b96c9cc83bdb3
Jon Badiali <jon@yottadb.com>
2025-07-17 16:49:42 -0400

    [#417][#450] Created documentation for new SimpleAPI/SimpleThreadApi functions
    Added documentation entries for new Simple API and Simple Thread API functions:
    + `ydb_encode_s()`
    + `ydb_encode_st()`
    + `ydb_decode_s()`
    + `ydb_decode_st()`
    
    Added new examples for the above the API functions:
    + `simple.c`: Implements example at https://gitlab.com/YottaDB/DB/YDB/-/issues/474#note_2594822569
    + `encode.c`: Implements a simple `ydb_encode_s` example per request at https://gitlab.com/YottaDB/DB/YDBDoc/-/merge_requests/880#note_2644622133
    + `users.c`: Implements Go program at https://gitlab.com/YottaDB/DB/YDBDoc/-/merge_requests/880#note_2635879334
    + `invalid.c`: Implements error case from https://gitlab.com/YottaDB/DB/YDBDoc/-/merge_requests/880#note_2641979508
    + `longkey.c`: Implements error case from https://gitlab.com/YottaDB/DB/YDBDoc/-/merge_requests/880#note_2641979508
    + `longvalue.c`: Implements error case from https://gitlab.com/YottaDB/DB/YDBDoc/-/merge_requests/880#note_2641979508

---
+++ MultiLangProgGuide/examples/invalid.c
@@ -0,0 +1,53 @@
+/****************************************************************
+ *								*
+ * Copyright (c) 2025 YottaDB LLC and/or its subsidiaries.	*
+ * All rights reserved.						*
+ *								*
+ *	This source code contains the intellectual property	*
+ *	of its copyright holder(s), and is made available	*
+ *	under a license.  If you do not know the terms of	*
+ *	the license, please stop and do not read further.	*
+ *								*
+ ****************************************************************/
+
+#include "libyottadb.h"	/* for ydb_* macros/prototypes/typedefs */
+
+#include <sys/types.h>	/* needed for "getpid" */
+#include <unistd.h>	/* needed for "getpid" */
+#include <stdio.h>	/* for "printf" */
+
+int main() {
+	int		status, done;
+	ydb_buffer_t	variable;
+	char		ydb_error[YDB_MAX_ERRORMSG];
+	const char	*format = "JSON";
+	const char	*vn = "^invalidJSON";
+	const char	*JSON = "{\n"
+					"\t\"1\": \"true\"\n"
+					"\t\"2\": \"false\",\n"
+					"\t\"\": 9,\n"
+					"\t\"three\": \"null\"\n"
+				"}";
+
+	status = ydb_init();
+	if (YDB_OK != status) {
+		ydb_zstatus(ydb_error, YDB_MAX_ERRORMSG);
+		printf("--> Error: ydb_init() [%s:%d] : %s\n", __FILE__, __LINE__, ydb_error);
+		return 1;
+	}
+
+	// Initialize YDB buffers for storing JSON in YottaDB
+	YDB_MALLOC_BUFFER(&variable, YDB_MAX_IDENT);
+	printf("# Decode JSON into YottaDB variable (%s)...\n", vn);
+	YDB_COPY_STRING_TO_BUFFER(vn, &variable, done);
+	YDB_ASSERT(done);
+	status = ydb_decode_s(&variable, 0, NULL, format, JSON);
+	if (YDB_OK != status) {
+		ydb_zstatus(ydb_error, YDB_MAX_ERRORMSG);
+		printf("--> Error: ydb_decode_s() [%s:%d] : %s\n", __FILE__, __LINE__, ydb_error);
+		YDB_FREE_BUFFER(&variable);
+		return 1;
+	}
+
+	return YDB_OK;
+}
